a52438740bd1db5768be5b2c04cd2923e4ebc373
Fasara <fabiana.asara@gmail.com>
2026-06-11 22:35:26 +0100

    Add lambda role

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -98,3 +98,29 @@ resource "aws_s3_bucket_policy" "frontend" {
     ]
   })
 }
+
+resource "aws_iam_role" "lambda_exec" {
+  name = "aws-demo-lambda-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+
+  tags = {
+    Project = "aws-demo-project"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
