afb87398de47c31eb435c282c802abcd644ec581
Fasara <fabiana.asara@gmail.com>
2026-06-11 21:34:16 +0100

    Create S3 bucket

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -11,3 +11,20 @@ terraform {
 provider "aws" {
   region = "eu-west-2"
 }
+
+resource "aws_s3_bucket" "frontend" {
+  bucket = "my-aws-demo-frontend"
+
+  tags = {
+    Project = "aws-demo-project"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "frontend" {
+  bucket = aws_s3_bucket.frontend.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
