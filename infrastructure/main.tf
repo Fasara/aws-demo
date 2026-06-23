d2dc9f581d0948de11b5ef9d99a8c7f1bb1abafa
Fasara <fabiana.asara@gmail.com>
2026-06-23 21:56:43 +0100

    undo migrate state

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -305,13 +305,3 @@ resource "aws_s3_bucket_public_access_block" "terraform_state" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
-
-terraform {
-  backend "s3" {
-    bucket       = "aws-demo-project-tfstate"
-    key          = "terraform.tfstate"
-    region       = "eu-west-2"
-    encrypt      = true
-    use_lockfile = true
-  }
-}
