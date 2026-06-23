b62f8bc9bf0145e4e8e0dd17190954450b740c1a
Fasara <fabiana.asara@gmail.com>
2026-06-23 21:28:13 +0100

    Create bucket to migrate terraform state

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -270,3 +270,38 @@ resource "aws_apigatewayv2_authorizer" "cognito" {
     issuer   = "https://cognito-idp.eu-west-2.amazonaws.com/${aws_cognito_user_pool.main.id}"
   }
 }
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "aws-demo-project-tfstate"  # must be globally unique across ALL of AWS, not just your account
+
+  tags = {
+    Project = "aws-demo-project"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
