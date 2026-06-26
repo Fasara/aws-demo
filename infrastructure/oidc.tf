7ac5e985141fe99952a46da030cb7e464cd01c3f
Fasara <fabiana.asara@gmail.com>
2026-06-26 12:15:30 +0100

    update action permission trust

--- infrastructure/oidc.tf
+++ infrastructure/oidc.tf
@@ -25,8 +25,10 @@ resource "aws_iam_role" "github_actions" {
             "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
           }
           StringLike = {
-            "token.actions.githubusercontent.com:sub" = "repo:Fasara/aws-demo:ref:refs/heads/main",
-            "token.actions.githubusercontent.com:sub" = "repo:Fasara/aws-demo:ref:refs/pull/*"
+            "token.actions.githubusercontent.com:sub" = [
+              "repo:Fasara/aws-demo:ref:refs/heads/main",
+              "repo:Fasara/aws-demo:pull_request"
+            ]
           }
         }
       }
