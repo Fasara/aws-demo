d668faefb03ee3b89537925afde49bf2d6955003
Fasara <fabiana.asara@gmail.com>
2026-06-28 19:51:04 +0100

    bucket permissions

--- infrastructure/oidc.tf
+++ infrastructure/oidc.tf
@@ -98,7 +98,9 @@ resource "aws_iam_role_policy" "github_actions_terraform" {
           "s3:DeleteBucketPolicy", "s3:GetBucketPublicAccessBlock", "s3:PutBucketPublicAccessBlock",
           "s3:GetBucketTagging", "s3:PutBucketTagging", "s3:GetBucketVersioning",
           "s3:GetBucketAcl", "s3:GetEncryptionConfiguration", "s3:GetBucketLocation", 
-          "s3:GetBucketCORS", "s3:GetBucketWebsite"
+          "s3:GetBucketCORS", "s3:GetBucketWebsite", "s3:GetAccelerateConfiguration",
+          "s3:GetBucketLogging", "s3:GetBucketRequestPayment", "s3:GetReplicationConfiguration",
+          "s3:GetLifecycleConfiguration", "s3:GetBucketObjectLockConfiguration", "s3:GetBucketNotification"
         ]
         Resource = aws_s3_bucket.frontend.arn
       },
